f4f9f9dbb5ce12591db82b1ee1824e4d6b1646e3
matthew.miranda174 <matthew.miranda174@my.tccd.edu>
2018-02-07 21:41:57 -0600

    Section 2 2/7/2018 Class and Methods

---
+++ Classwork/Section2/Nile.Windows/MainForm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Nile.Windows
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        protected override void OnLoad( EventArgs e )
+        {
+            base.OnLoad(e);
+
+            var product = new Product();
+
+            var name = product.GetName();
+            //product.Name = "Product A";
+            product.SetName("Product A");
+            //product.Description = "Name";
+
+            var error = product.Validate();
+
+            var str = product.ToString();
+
+            var productB = new Product();
+            //productB.Name = "Product B";
+            //productB.SetName("Product B");
+            //productB.Description = product.Description;
+            error = productB.Validate();
+        }
+    }
+}
---
+++ Classwork/Section2/Nile/Product.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nile
+{
+    /// <summary> Provides information about a product.</summary>
+    public class Product
+    {
+        internal decimal discountPercentage = 0.10M;
+
+        
+      
+/// <summary>Get the product name.
+/// <returns>The name.</returns>
+        public string GetName ()
+        {
+            return _name ?? "";
+        }
+
+        /// <summary>
+        /// Sets the product Name.</summary>
+        /// <param name="value">The name.</param>
+        public void SetName (string value)
+        {
+            _name = value ?? "";
+        }
+
+        /// <summary>
+        /// Validates the product
+        /// </summary>
+        /// <returns>Error Message, if any</returns>
+        public string Validate()
+        {
+            // Name is required
+            if (String.IsNullOrEmpty(_name))
+                return "Name Cannot be empty";
+
+            // Price >= 0
+            if (_price < 0)
+                return "price must be >= 0";
+
+            return "";
+        }
+
+
+
+        /// <summary>
+        /// Not local variables, they are fields. Outside of the function it is a field.
+        /// Fields are always Private. Generally camel case.
+        /// </summary>
+        private string _name = "";
+        private string _description;
+        private decimal _price;
+        private bool _isDiscontinued;
+    }
+}
